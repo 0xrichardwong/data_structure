e5be898bfc90088502e60a9195cd595dbc298d1e
richardwong_ <shigenkawasaki@gmail.com>
2024-06-15 15:50:14 +0900

    Add files via upload

--- singly_linked_list.js
+++ singly_linked_list.js
@@ -1,5 +1,5 @@
-class Node{
-    constructor(data){
+class SinglyLinkedListNode{
+    constructor(data) {
         this.data = data;
         this.next = null;
     }
@@ -7,34 +7,55 @@ class Node{
 
 class SinglyLinkedList{
     constructor(arr){
-        this.head = arr.length >0? new Node(arr[0]) : new Node(null);
-        
+        this.head = arr.length>0? new SinglyLinkedListNode(arr[0]) : null;
         let currentNode = this.head;
-        for(let i=1; i<arr.length; i++){
-            currentNode.next = new Node(arr[i]);
+        for(let i=1; i<arr.length;i++){
+            currentNode.next = new SinglyLinkedListNode(arr[i]);
             currentNode = currentNode.next;
-        }        
-    }
-
-    at(index){
-        let iterator = this.head;
-        for(let i=1; i<index; i++){
-            iterator = iterator.next;
-            if(iterator === null) return null;
         }
-        return iterator;
     }
 
     printList(){
         let iterator = this.head;
-        let output = '';
+        let output;
         while(iterator !== null){
-            output += iterator.data + " ";
+            output += iterator.data;
             iterator = iterator.next;
         }
+        output += "END";
         return output;
     }
+
+    preAppend(data){
+        let firstNode = new SinglyLinkedListNode(data);
+        firstNode.next = this.head;
+        this.head = firstNode;
+    }
+
+    append(data){
+        let iterator = this.head;
+        while(iterator.next !== null){
+            iterator = iterator.next;
+            
+        }
+        iterator.next = new SinglyLinkedListNode(data);
+
+    }
+    
 }
 
-// let list = new SinglyLinkedList([1,2,3,4,5]);
-// console.log(list.printList());
+/*
+function insertAtHead(head,data){
+    let list = new SinglyLinkedList(head);
+    list.preAppend(data);
+    return list.printList();
+}
+console.log(insertAtHead([1,2,3],5))
+*/
+
+function insertAtTail(tail,data){
+    let list = new SinglyLinkedList(tail);
+    list.append(data);
+    return list.printList();
+}
+console.log(insertAtTail([3,3,2,10,34,45,67,356],367));
